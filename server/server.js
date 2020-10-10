3fd89031f038593e94dc9e42126b76eadb27164d
grahse <gyykim@gmail.com>
2020-10-10 10:50:07 -0700

    Connect campaign and community modules

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3010;
-const cors = require('cors');
 
 app.use(express.static('client/dist'));
+
+// app.get('/:itemId', (req, res) => {
+//   if (typeof req.params.itemId !== 'number') {
+//     res.status(400);
+//   } else if (req.params.itemdId > 100) {
+//     res.status(400);
+//   } else {
+//     res.status(200);
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Proxy listening at http://localhost:${port}`);
