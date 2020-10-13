592b08f69cccb2957c5de00ef457b4bbae52fb8e
grahse <gyykim@gmail.com>
2020-10-12 17:04:36 -0700

    Add route serving static files for itemId

--- server/server.js
+++ server/server.js
@@ -2,17 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3010;
 
-app.use(express.static('client/dist'));
-
-// app.get('/:itemId', (req, res) => {
-//   if (typeof req.params.itemId !== 'number') {
-//     res.status(400);
-//   } else if (req.params.itemdId > 100) {
-//     res.status(400);
-//   } else {
-//     res.status(200);
-//   }
-// });
+app.use('/:itemId', express.static('client/dist'));
 
 app.listen(port, () => {
   console.log(`Proxy listening at http://localhost:${port}`);
